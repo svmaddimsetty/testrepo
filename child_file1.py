1d7832f5034e6cadde44e67c0cdf8c10d9f40216
Swamy <svmaddimsetty@gmail.com>
2021-08-28 12:41:23 -0400

    Commit

--- child_file1.py
+++ child_file1.py
@@ -1,1 +1,2 @@
+
 print("child branch file")
--- firstpython.py
+++ firstpython.py
@@ -1,6 +1,7 @@
-string1 = "Hello World"
+string1 = "Hello World , this is conflict"
 print(string1)
 print("My name is Swamy!")
 print("My second line insrted locally")
 print("My third line after git commit")
 print("My fourth line in github")
+print("I am doing changes in  bash now")
